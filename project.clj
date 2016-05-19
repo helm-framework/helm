96818528f45ab2c917a2e6973ac23fdfe574c841
Jordan Schatz <jordan@noionlabs.com>
2016-05-18 21:48:09 -0700

    Update tagline

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject helm/lein-template "0.1.0-SNAPSHOT"
-  :description "Helm"
+  :description "Helm -- The web framework for pragmatic professionals"
   :url "https://github.com/helm-framework/helm"
   :license {:name "MIT + Do no evil" :url "http://www.json.org/license.html"}
   :eval-in-leiningen true)
